f269ea71499bd3593692d0a5e5bc95c6cd68cbd3
greentm372@gmail.com <greentm372@gmail.com>
2024-03-08 12:03:44 +0800

    makeW

---
+++ Plugins_loader/Plugins/Form1.cs
@@ -0,0 +1,66 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+
+
+namespace Plugins
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Assuming Properties.Resources.WindowsFormsApp1 is a byte array representing the assembly
+            ModuleDefMD asmDef = ModuleDefMD.Load(Properties.Resources.WindowsFormsApp1);
+
+            WriteSettings.WriteSetting(asmDef, textBox1.Text);
+            using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+            {
+                saveFileDialog1.Filter = ".exe (*.exe)|*.exe";
+                saveFileDialog1.InitialDirectory = Application.StartupPath;
+                saveFileDialog1.OverwritePrompt = false;
+                saveFileDialog1.FileName = "main";
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+                    try
+                    {
+                        asmDef.Write(saveFileDialog1.FileName);
+                        asmDef.Dispose();
+                        MessageBox.Show("Assembly modified and saved to: " + saveFileDialog1.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Plugins_loader/Plugins/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plugins
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+    }
+}
